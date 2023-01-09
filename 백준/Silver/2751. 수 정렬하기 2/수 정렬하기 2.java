d52fe8916f8e159588579e0da13a57c2682f64ab
xoals25 <68364917+xoals25@users.noreply.github.com>
2023-01-10 00:52:23 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 816 ms, Memory: 94720 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,22 @@
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
-import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[2_000_001];
-        
-        for (int i = 0; i < n; i++) {
-            arr[Integer.parseInt(br.readLine()) + 1_000_000]++;
+        int N = Integer.parseInt(br.readLine());
+        boolean [] arr = new boolean[2000001];
+        for(int i = 0 ; i<N; i++){
+            arr[Integer.parseInt(br.readLine()) + 1000000] = true;
         }
-        
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] != 0) {
-                bw.write(i - 1_000_000 + "\n");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0 ; i<arr.length; i++){
+            if(arr[i]){
+                sb.append(i-1000000).append("\n");
             }
         }
-        
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 }
