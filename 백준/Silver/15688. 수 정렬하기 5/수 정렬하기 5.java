5aed75cc45703b6107d1832e1f04ac90d24645fc
xoals25 <68364917+xoals25@users.noreply.github.com>
2023-01-10 01:36:48 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 15864 ms, Memory: 135924 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -9,55 +9,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int[] tmp = new int[n];
+        int[] arr = new int[2_000_001];
         
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[Integer.parseInt(br.readLine()) + 1_000_000]++;
         }
         
-        mergeSort(arr, tmp, 0, n - 1);
-        
         for (int i = 0; i < arr.length; i++) {
-            bw.write(arr[i] + "\n");
+            for (int j = 0; j < arr[i]; j++) {
+                bw.write(i - 1_000_000 + "\n"); 
+            }
         }
         
         bw.flush();
         bw.close();
         br.close();
-    }
-    
-    public static void mergeSort(int[] arr, int[] tmp, int left, int right) {
-        if (left >= right) {
-            return;
-        }
-        int mid = left + (right - left) / 2;
-        mergeSort(arr, tmp, left, mid);
-        mergeSort(arr, tmp, mid + 1, right);
-        merge(arr, tmp, left, right, mid);
-    }
-    
-    public static void merge(int[] arr, int[] tmp, int left, int right, int mid) {
-        int p = left;
-        int q = mid + 1;
-        int idx = p;
-        
-        while (p <= mid || q <= right) {
-            if (p <= mid && q <= right) {
-                if (arr[p] <= arr[q]) {
-                    tmp[idx++] = arr[p++];
-                } else {
-                    tmp[idx++] = arr[q++];
-                }
-            } else if (p <= mid && q > right) {
-                tmp[idx++] = arr[p++];
-            } else {
-                tmp[idx++] = arr[q++];
-            }
-        }
-        
-        for (int i = left; i <= right; i++) {
-            arr[i] = tmp[i];
-        }
     }
 }
