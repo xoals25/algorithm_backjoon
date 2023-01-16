aad42264e10975c2a4a76c04fe4abdd6ce90274b
xoals25 <68364917+xoals25@users.noreply.github.com>
2023-01-16 22:39:04 +0900

    [Silver V] Title: 소수 찾기, Time: 148 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int count = 0;
+
+        for (int i = 0; i < T; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (isPrime(num)) {
+                count++;
+            }
+        }
+        
+        
+        
+        bw.write(count + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        } else if (n <= 3) {
+            return true;
+        } else if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+
+        for (int i = 5; i * i <= n; i = i + 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
