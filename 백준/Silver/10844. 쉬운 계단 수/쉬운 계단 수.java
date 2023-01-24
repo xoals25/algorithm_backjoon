06a8e83c1b7ffa58c86883adde1e0872ecf41d06
xoals25 <68364917+xoals25@users.noreply.github.com>
2023-01-24 11:20:46 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 140 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -11,14 +11,17 @@ public class Main {
         int T = Integer.parseInt(br.readLine());
         long[][] dp = new long[T + 1][10];
         long devide = 1_000_000_000;
-        for (int i = 1; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
+            dp[0][i] = 1;
             dp[1][i] = 1;
         }
         
         for (int i = 2; i <= T; i++) {
-            dp[i][0] = dp[i - 1][1];
             for (int j = 1; j <= 9; j++) {
-               if (j == 9) {
+                if (j == 1) {
+                    long d = dp[i - 2][j] == 0 ? 1 : dp[i - 2][j];
+                    dp[i][j] = (dp[i - 1][j + 1] + d) % devide;
+                } else if (j == 9) {
                     dp[i][j] = (dp[i - 1][j - 1]) % devide;
                 } else {
                     dp[i][j] = (dp[i - 1][j - 1] % devide + dp[i - 1][j + 1] % devide) % devide;
@@ -27,7 +30,7 @@ public class Main {
         }
         
         long sum = 0;
-        for (int i = 0; i <= 9; i++) {
+        for (int i = 1; i <= 9; i++) {
             sum = (sum + dp[T][i]) % devide;
         }
         
