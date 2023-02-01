2fd4b58a635152672d2625390a82711c613053ae
xoals25 <68364917+xoals25@users.noreply.github.com>
2023-02-01 10:32:14 +0900

    [Gold III] Title: 웜홀, Time: 452 ms, Memory: 47316 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,77 +1,107 @@
-import java.util.stream.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-import static java.util.Arrays.*;
-import static java.util.stream.Collectors.*;
-
-import java.util.*;
-import java.io.*;
-
+class Node{
+    int end;
+    int weight;
+	
+    public Node(int end, int weight) {
+        this.end = end;
+        this.weight = weight;
+    }
+}
 
 public class Main {
+	
+    static final int INF = 1_000_000_000;
+    static ArrayList<Node>[] nodeList;
 
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-
-        while (tc-->0){
-             int[] input = stream(br.readLine().split(" "))
-                      .mapToInt(Integer::parseInt).toArray();
-             n = input[0];
-             int m = input[1], w= input[2];
-
-            graph = new ArrayList<>();
-            IntStream.rangeClosed(0,n).forEach(e->graph.add(new ArrayList<>()));
-
-            for(int i=0;i<m+w;i++){
-                 input = stream(br.readLine().split(" "))
-                          .mapToInt(Integer::parseInt).toArray();
-                if(i<m){
-                    graph.get(input[0]).add(new Node(input[1],input[2]));
-                    graph.get(input[1]).add(new Node(input[0],input[2]));
-                }else
-                    graph.get(input[0]).add(new Node(input[1],-1*input[2]));
-            }//end input
-
-            if(bellmanford())
-                System.out.println("YES");
-            else
-                System.out.println("NO");
-
+		
+        int T = Integer.parseInt(br.readLine());
+		
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+			
+            int N = Integer.parseInt(st.nextToken()); // 지점의 개수
+            int M = Integer.parseInt(st.nextToken()); // 도로의 개수
+            int W = Integer.parseInt(st.nextToken()); // 웜홀의 개수
+			
+            nodeList = new ArrayList[N + 1];
+			
+            for (int j = 1; j < N + 1; j++) {
+                nodeList[j] = new ArrayList<>();
+            }
+			
+            for (int j = 0; j < M + W; j++) {
+                st = new StringTokenizer(br.readLine());
+				
+                int S = Integer.parseInt(st.nextToken()); // 시작 지점
+                int E = Integer.parseInt(st.nextToken()); // 도착 지점
+                int time = Integer.parseInt(st.nextToken()); // 줄어드는 시간
+				
+                if (j < M) {
+                    // 도로는 방향이 없기 때문에 둘 다 추가해줌
+                    nodeList[S].add(new Node(E, time));
+                    nodeList[E].add(new Node(S, time));
+                }
+                else {
+                    // 웜홀은 방향이 있지만 웜홀을 지나치는 경우 시간이 거꾸로 가기 때문에 음수로 바꾸어서 추가해줌
+                    nodeList[S].add(new Node(E, -time));
+                }
+            }
+			
+            // 음수 사이클이 발생한 경우 YES, 아닌 경우 NO를 출력
+            System.out.println(bellman(N) ? "YES" : "NO");
         }
-
+		
     }
-
-    private static boolean bellmanford(){
-
-        int[] distance = new int[n+1]; // distance 배열 0으로 초기화
-
-        boolean update =false;
-
-        for(int i=1; i<=n; i++){ // 수행횟수 : n-1(다익스트라) +1(최종확인) = n번
-            update =false;
-            for(int cur=1; cur<=n; cur++){  
-
-                for (Node next : graph.get(cur)) {
-                    if(distance[next.node] > distance[cur]+next.weight){
-                        distance[next.node] = distance[cur]+next.weight;
-                        update = true; 
+	
+    public static boolean bellman(int n) {
+        boolean isUpdate = false;
+        int[] dist = new int[n + 1];
+		
+		
+        // 모든 지점을 N - 1번 순회
+        for (int i = 1; i < n; i++) {
+            isUpdate = false;
+			
+            // 모든 간선을 순회하는 for문
+            // 음수 사이클이 있는 경우 N번째에서도 최단 거리를 찾아내기 때문에 for문을 N번까지 돌려보고 업데이트 되는지 확인해야 한다.
+            // 만약 N번째에서 사이클이 발생할 경우에는 isUpdate이 다시 false로 변경되지 않기 때문에 true일 경우에는 사이클이 발생한 것
+            for (int j = 1; j <= n; j++) {
+                for (Node node : nodeList[j]) {
+                    if (dist[node.end] > dist[j] + node.weight) {
+                        dist[node.end] = dist[j] + node.weight;
+                        isUpdate = true;
                     }
                 }
             }
-            if(!update) // 음의 사이클이 없는 경우
+			
+            // 진행 중에 최단 거리가 하나라도 갱신되지 않을 경우 반복문 종료
+            if (!isUpdate) {
                 break;
+            }
         }
-        return update;
+		
+        // 사이클이 발생한 경우 true 반환
+        if (isUpdate) {
+            for (int j = 1; j <= n; j++) {
+                for (Node node : nodeList[j]) {
+                    if (dist[j] != INF && dist[node.end] > dist[j] + node.weight) {
+                        dist[node.end] = dist[j] + node.weight;
+                        return true;
+                    }
+                }
+            }
+        }
+		
+        return false;
     }
 
-    static class Node{
-        int node,weight;
-
-        public Node(int node, int weight) {
-            this.node = node;
-            this.weight = weight;
-        }
-    }
 }
