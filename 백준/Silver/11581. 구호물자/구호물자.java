c644978a9b50c6eb2511521ad9fed9d93aa5d8ac
xoals25 <68364917+xoals25@users.noreply.github.com>
2023-02-08 14:22:05 +0900

    [Silver I] Title: 구호물자, Time: 136 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Silver/11581. 구호물자/구호물자.java
+++ 백준/Silver/11581. 구호물자/구호물자.java
@@ -3,17 +3,20 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
+import java.util.Stack;
 
 public class Main {
     static int n; // 교차로의 수 (정점 수)
     static boolean[][] arr;
+    static int[] visited;
+    static String ans;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         n = Integer.parseInt(br.readLine());
         arr = new boolean[n + 1][n + 1];
-
+        visited = new int[n + 1];
         for (int i = 1; i < n; i++) {
             int e = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -23,28 +26,29 @@ public class Main {
                 arr[i][num] = true;
             }
         }
-
-        for (int k = 1; k < n; k++) {
-            for (int i = 1; i < n; i++) {
-                for (int j = 1; j < n; j++) {
-                    if (arr[i][k] && arr[k][j]) {
-                        arr[i][j] = true;
-                    }
-                }
-            }
-        }
-
-        String ans = "NO CYCLE";
-        for (int i = 1; i < n; i++) {
-            if (arr[1][i] && arr[i][i]) {
-                ans = "CYCLE";
-                break;
-            }
-        }
+        
+        ans = "NO CYCLE";        
+        dfs(1);
 
         bw.write(ans + "\n");
         bw.flush();
         bw.close();
         br.close();
+    }
+    public static void dfs(int v) {
+        visited[v] = 1;
+        
+        for (int i = 1; i < n + 1; i++) {
+            if (arr[v][i]) {
+                if (visited[i] == 0) {
+                    dfs(i);
+                } else if (visited[i] == 1) {
+                    ans = "CYCLE";
+                    return;
+                }
+            }
+        }
+        
+        visited[v] = 2;
     }
 }
