2bd596d00c9a58621a7bdfc689f8f42f67cc3521
xoals25 <68364917+xoals25@users.noreply.github.com>
2023-02-28 21:55:29 +0900

    [Silver V] Title: 거스름돈, Time: 124 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int LNF = Integer.MAX_VALUE;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n + 1 + 6];
+        arr[0] = LNF;
+        arr[1] = LNF;
+        arr[2] = 1;
+        arr[3] = LNF;
+        arr[4] = 2;
+        arr[5] = 1;
+
+        for (int i = 6; i < arr.length; i++) {
+            arr[i] = Math.min(arr[i - 2], arr[i - 5]) + 1;
+        }
+
+        System.out.println(arr[n] == LNF ? -1 : arr[n]);
+        br.close();
+    }
+}
