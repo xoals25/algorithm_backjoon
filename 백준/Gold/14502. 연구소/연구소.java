883aae917e16f4b43b44c09e65b54253ab5f19fa
xoals25 <68364917+xoals25@users.noreply.github.com>
2023-03-22 18:22:44 +0900

    [Gold IV] Title: 연구소, Time: 244 ms, Memory: 25152 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,115 +1,80 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N; // 행의 개수 (세로 길이)
-    static int M; // 열의 개수 (가로 길이)
-    static int[][] board;
-    static int zeroCount = 0;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static int ans;
+    static int[][] arr = new int[8][8];
+    static int[] dx = { 0, 0, 1, -1 };
+    static int[] dy = { 1, -1, 0, 0 };
+    static int virusNum, voidArea;
+    static int[][] q = new int[64][2];
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        board = new int[N][M];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-
-            for (int j = 0; j < M; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
-
-                if (board[i][j] == 0) {
-                    zeroCount++;
-                }
+    static int bfs() {
+        int front = 0;
+        int rear = virusNum;
+        int res = voidArea - 3;
+        boolean[][] visited = new boolean[8][8];
+        while (front < rear) {
+            int x = q[front][0];
+            int y = q[front++][1];
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M)
+                    continue;
+                if (arr[nx][ny] != 0 || visited[nx][ny])
+                    continue;
+                visited[nx][ny] = true;
+                q[rear][0] = nx;
+                q[rear++][1] = ny;
+                res--;
             }
         }
-        int max = 0;
-        for (int i = 0; i < N; i++) {
-            for (int _i = 0; _i < M; _i++) {
-                if (board[i][_i] == 0) {
-                    board[i][_i] = 1;
-
-                    for (int j = 0; j < N; j++) {
-                        for (int _j = 0; _j < M; _j++) {
-                            if (board[j][_j] == 0) {
-                                board[j][_j] = 1;
-
-                                for (int k = 0; k < N; k++) {
-                                    for (int _k = 0; _k < M; _k++) {
-                                        if (board[k][_k] == 0) {
-                                            board[k][_k] = 1;
-                                            int count = birusCountBfs();
-                                            max = Math.max(max, zeroCount - count - 3);
-                                            board[k][_k] = 0;
-                                        }
-                                    }
-                                }
-
-                                board[j][_j] = 0;
-                            }
-                        }
-                    }
-
-                    board[i][_i] = 0;
-                }
-            }
-        }
-        System.out.println(max);
-        br.close();
+        return res;
     }
 
-    public static int birusCountBfs() {
-        int count = 0;
+    static void dfs(int cnt, int index) {
+        if (cnt == 3) {
+            int res = bfs();
+            if (res > ans)
+                ans = res;
+            return;
+        }
+        for (int i = index; i < N * M; i++) {
+            int x = i / M;
+            int y = i % M;
+            if (arr[x][y] != 0)
+                continue;
+            arr[x][y] = 1;
+            dfs(cnt + 1, i + 1);
+            arr[x][y] = 0;
+        }
+    }
 
-        Queue<int[]> q = new LinkedList<>();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
 
         for (int i = 0; i < N; i++) {
+            token = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                if (board[i][j] == 2) {
-                    q.offer(new int[]{i, j});
+                arr[i][j] = Integer.parseInt(token.nextToken());
+                if (arr[i][j] == 2) {
+                    q[virusNum][0] = i;
+                    q[virusNum++][1] = j;
+                }
+                if (arr[i][j] == 0) {
+                    voidArea++;
                 }
             }
         }
 
-        boolean[][] visited = new boolean[N][M];
+        dfs(0, 0);
 
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-
-            int x = cur[0];
-            int y = cur[1];
-
-            if (visited[x][y]) {
-                continue;
-            }
-
-            if (board[x][y] == 0) {
-                count++;
-            }
-
-            visited[x][y] = true;
-
-            if (x - 1 >= 0 && !visited[x - 1][y] && board[x - 1][y] == 0) {
-                q.offer(new int[]{x - 1, y});
-            }
-
-            if (x + 1 < N && !visited[x + 1][y] && board[x + 1][y] == 0) {
-                q.offer(new int[]{x + 1, y});
-            }
-
-            if (y - 1 >= 0 && !visited[x][y - 1] && board[x][y - 1] == 0) {
-                q.offer(new int[]{x, y - 1});
-            }
-
-            if (y + 1 < M && !visited[x][y + 1] && board[x][y + 1] == 0) {
-                q.offer(new int[]{x, y + 1});
-            }
-        }
-
-        return count;
+        System.out.println(ans);
     }
 }
