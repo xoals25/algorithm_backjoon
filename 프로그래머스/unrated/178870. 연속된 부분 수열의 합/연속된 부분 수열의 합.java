b1dbac3114eff77fbad92d142ba00ae290557aa2
xoals25 <68364917+xoals25@users.noreply.github.com>
2023-07-07 22:56:17 +0900

    [unrated] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] solution(int[] sequence, int k) {
+        int[] answer = new int[]{0, sequence.length - 1};
+    
+        int left = 0;
+        int right = 1;
+        
+        int sum = sequence[0];
+        
+        while(left < right) {
+            if (sum == k) {
+                change(right, left, answer);
+                sum -= sequence[left++];
+                
+                if (right < sequence.length) {
+                    sum += sequence[right++];
+                } else {
+                    break;
+                }
+            } else if (sum > k) {
+                sum -= sequence[left++];
+            } else {
+                if (right < sequence.length) {
+                    sum += sequence[right++];
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+  
+    private void change(int right, int left, int[] answer) {
+        if (right - 1 - left < answer[1] - answer[0]) {
+                answer[0] = left;
+                answer[1] = right - 1;
+        }
+    }
+}
